7dcaf1bba85e616b6ad3617d7feb29a3d878b78b
Dustin Lacewell <dustin@contactmaverick.com>
2011-03-31 14:48:36 -0500

    Works\!

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+from ark import VERSION
+
+setup(
+    name='capn',
+    version=VERSION,
+#    packages=['capn',],
+    scripts=['bin/capn', 'bin/__capn'],
+    license='Creative Commons Attribution-Noncommercial-Share Alike license',
+    long_description=open('README').read(),
+)
