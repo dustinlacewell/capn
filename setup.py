4428ec96820c7a4f869f0e984f183df56cdedb8c
Dustin Lacewell <dlacewell@gmail.com>
2011-04-05 15:40:48 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,9 +2,10 @@ from distutils.core import setup
 
 setup(
     name='capn',
-    version='1.0.1',
+    version='1.0.2',
     packages=['capn',],
     scripts=['bin/capn', 'bin/__capn'],
+    data_files=['README.markdown',],
     license='Creative Commons Attribution-Noncommercial-Share Alike license',
     long_description=open('README.markdown').read(),
 )
