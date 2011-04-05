a984be71a846054af6d0909e3c77f454b7a033f2
Dustin Lacewell <dlacewell@gmail.com>
2011-04-05 15:58:59 -0500

    Added manifest file

--- setup.py
+++ setup.py
@@ -2,10 +2,9 @@ from distutils.core import setup
 
 setup(
     name='capn',
-    version='1.0.2',
+    version='1.0.4',
     packages=['capn',],
     scripts=['bin/capn', 'bin/__capn'],
-    data_files=['README.markdown',],
     license='Creative Commons Attribution-Noncommercial-Share Alike license',
-    long_description=open('README.markdown').read(),
+    long_description=open("README.markdown", 'r').read(),
 )
