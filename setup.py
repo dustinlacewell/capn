32acc64f6b8cfe1dea24848aa3bacc7f64d71b3b
Dustin Lacewell <dlacewell@gmail.com>
2011-04-05 15:30:57 -0500

    Updated version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='capn',
-    version='1.0',
+    version='1.0.1',
     packages=['capn',],
     scripts=['bin/capn', 'bin/__capn'],
     license='Creative Commons Attribution-Noncommercial-Share Alike license',
