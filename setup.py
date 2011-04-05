43d784fd6f27b285ce4d2a9d08d2096bbbf578e1
Dustin Lacewell <dlacewell@gmail.com>
2011-04-05 16:16:11 -0500

    Updated packaging

--- setup.py
+++ setup.py
@@ -2,9 +2,12 @@ from distutils.core import setup
 
 setup(
     name='capn',
-    version='1.0.4',
+    version='1.0.6',
     packages=['capn',],
     scripts=['bin/capn', 'bin/__capn'],
-    license='Creative Commons Attribution-Noncommercial-Share Alike license',
+    author="Dustin Lacewell",
+    author_email="dlacewell@gmail.com",
+    url="https://github.com/dustinlacewell/capn",
+    description="capn provides hooks on working-directory change.",
     long_description=open("README.markdown", 'r').read(),
 )
